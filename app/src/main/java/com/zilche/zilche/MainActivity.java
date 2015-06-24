56d7009c41cef8323fde72a725a0caefdfaea1bc
SQuare <noahfangchen@gmail.com>
2015-06-24 18:24:37 -0400

    Action Bar Finished

--- app/src/main/java/com/zilche/zilche/MainActivity.java
+++ app/src/main/java/com/zilche/zilche/MainActivity.java
@@ -50,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_list) {
+
             return true;
         }
 
