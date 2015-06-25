46c1b9c3603790ff9554882ecf5bb9c9ac402351
SQuare <noahfangchen@gmail.com>
2015-06-25 18:47:54 -0400

    Alternate to ToolBar

--- app/src/main/java/com/zilche/zilche/MainActivity.java
+++ app/src/main/java/com/zilche/zilche/MainActivity.java
@@ -74,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         return super.onOptionsItemSelected(item);
     }
 
