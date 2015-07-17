15309a0f0fce2c88290a1bebf7ad72edefb8040a
Mark Yang <markyang13705@gmail.com>
2015-07-17 00:06:46 -0400

    Minor fix to categories in create survey activity

--- app/src/main/java/com/zilche/zilche/CreateSurveyActivity.java
+++ app/src/main/java/com/zilche/zilche/CreateSurveyActivity.java
@@ -500,15 +500,16 @@ public class CreateSurveyActivity extends FragmentActivity implements OnPageChan
         public class ImageAdapter extends BaseAdapter {
 
             private Context c;
-            private Integer[] thumbnails = {
-                    R.drawable.category_all, R.drawable.category_auto, R.drawable.category_enter, R.drawable.category_fashion, R.drawable.category_food,
-                    R.drawable.category_games, R.drawable.category_it, R.drawable.category_pets, R.drawable.category_sci, R.drawable.category_sports,
-                    R.drawable.category_social, R.drawable.category_tech, R.drawable.category_travel
+            private int[] thumbnails = {
+                    R.drawable.category_all, R.drawable.category_auto, R.drawable.category_edu, R.drawable.category_enter,
+                    R.drawable.category_fash, R.drawable.category_finance, R.drawable.category_food, R.drawable.category_games,
+                    R.drawable.category_it, R.drawable.category_pets, R.drawable.category_sci, R.drawable.category_social,
+                    R.drawable.category_sports, R.drawable.category_tech, R.drawable.category_travel
             };
-            private Integer[] strings = {
-                    R.string.category_all, R.string.category_auto, R.string.category_entertainment, R.string.category_fashion,
-                    R.string.category_food, R.string.category_games, R.string.category_it,
-                    R.string.category_pet, R.string.category_science, R.string.category_sports, R.string.category_social, R.string.category_tech,
+            private int[] strings = {
+                    R.string.category_all, R.string.category_auto, R.string.category_education, R.string.category_entertainment,
+                    R.string.category_fashion, R.string.category_finance, R.string.category_food, R.string.category_games, R.string.category_it,
+                    R.string.category_pet, R.string.category_science, R.string.category_social, R.string.category_sports, R.string.category_tech,
                     R.string.category_travel
             };
             private int[] bg_color = {
