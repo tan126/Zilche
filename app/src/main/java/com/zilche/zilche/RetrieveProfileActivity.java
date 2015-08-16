0f112e5840686b382a180dcaaf097e883be489b6
Ho57 <ho57@purdue.edu>
2015-08-16 00:32:57 -0400

    user activity fix

--- app/src/main/java/com/zilche/zilche/RetrieveProfileActivity.java
+++ app/src/main/java/com/zilche/zilche/RetrieveProfileActivity.java
@@ -8,6 +8,9 @@ import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -24,6 +27,9 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.support.v4.app.Fragment;
@@ -38,6 +44,8 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -73,7 +81,7 @@ public class RetrieveProfileActivity extends FragmentActivity implements ViewPag
 
         TextView usernameText;
         usernameText = (TextView) findViewById(R.id.userName);
-        ParseUser currentUser = ParseUser.getCurrentUser();
+        //ParseUser currentUser = ParseUser.getCurrentUser();
         usernameText.setText(getIntent().getStringExtra("authorRealName"));
 
 
@@ -248,18 +256,216 @@ public class RetrieveProfileActivity extends FragmentActivity implements ViewPag
     public static class SecondFragment extends Fragment {
 
 
+
+        private ImageButton back_button;
+        private SearchView sv;
+        private RecyclerView rv;
+        private LinkedList<Poll> pollList;
+        private ProgressBar spinner;
+        private int isRefreshing = 0;
+        private HashMap<String, Integer> map;
+        private int skip = 0;
+        private int complete = 0;
+        private int visibleThreshold = 15;
+        private boolean load = false;
+        //private String str;
+        //private String userNameTest;
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View v = inflater.inflate(R.layout.profile_activity, container, false);
 
+            Zilche app = (Zilche) getActivity().getApplication();
+            map = app.getMap();
+            spinner = (ProgressBar) v.findViewById(R.id.progress_bar);
 
+            populateList(skip);
+
+            rv = (RecyclerView) v.findViewById(R.id.rv_cat);
+            rv.setHasFixedSize(true);
+            final LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+            rv.setLayoutManager(llm);
+            pollList = new LinkedList<>();
+            RVadapter rva = new RVadapter(pollList);
+            rv.setAdapter(rva);
+
+
+
+            rv.setOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    int firstVisibleItem = llm.findFirstVisibleItemPosition();
+                    if (!load && ((pollList.size() > 0 && pollList.size() - firstVisibleItem <= visibleThreshold) || !rv.canScrollVertically(1))) {
+                        if (complete == 0) {
+                            if (pollList.size() == 0 || pollList.getLast().getId().compareTo("-1") != 0) {
+                                Poll tmp = new Poll();
+                                tmp.setId("-1");
+                                pollList.add(tmp);
+                                rv.getAdapter().notifyDataSetChanged();
+                            }
+                            if (pollList.size() != 0)
+                                populateList(skip);
+                        }
+                    }
+
+                }
+            });
 
             return v;
+        }
+
+        public void populateList(final int skip2) {
+            load = true;
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("poll");
+            query.whereEqualTo("author", getActivity().getIntent().getExtras().getString("authorName"));
+            query.whereNotEqualTo("anon", 1);
+            query.setLimit(15);
+            query.whereNotEqualTo("archived", 1);
+            query.setSkip(skip2 * 15);
+            query.orderByDescending("lastUpdate");
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> list, ParseException e) {
+                    if (e == null) {
+                        if (pollList.size() != 0 && pollList.getLast() != null && pollList.getLast().getId().compareTo("-1") == 0) {
+                            pollList.removeLast();
+                            rv.getAdapter().notifyDataSetChanged();
+                        }
+                        if (list.size() < 15) {
+                            complete = 1;
+                        } else {
+                            complete = 0;
+                        }
+                        if (list.size() != 0) {
+                            if (skip2 == 0) {
+                                pollList.clear();
+                                spinner.setVisibility(View.VISIBLE);
+                            }
+                            for (int i = 0; i < list.size(); i++) {
+                                pollList.add(Util.parsePollObject(list.get(i)));
+                            }
+                            rv.getAdapter().notifyDataSetChanged();
+                            skip++;
+                        } else {
+                            pollList.clear();
+                            rv.getAdapter().notifyDataSetChanged();
+                        }
+                        load = false;
+                    } else {
+                        if (pollList.size() != 0 && pollList.getLast() != null && pollList.getLast().getId().compareTo("-1") == 0) {
+                            pollList.removeLast();
+                            rv.getAdapter().notifyDataSetChanged();
+                        }
+                        load = false;
+                        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    if (spinner.getVisibility() == View.VISIBLE) {
+                        spinner.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
+
+        public class RVadapter extends RecyclerView.Adapter<RVadapter.PollViewHolder> {
+
+            List<Poll> polls;
+            View.OnClickListener onclick = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final int pos = (int) v.getTag();
+                    Intent i = new Intent(getActivity(), PollViewActivity.class);
+                    i.putExtra("poll", polls.get(pos));
+                    startActivity(i);
+                    //overridePendingTransition(R.anim.right_to_left, 0);
+                }
+
+            };
+
+            public class PollViewHolder extends RecyclerView.ViewHolder{
+
+                LinearLayout main;
+                ProgressBar pb;
+                CardView cv;
+                TextView question;
+                TextView date;
+                TextView total_votes;
+                ImageView iv;
+                ImageView has_photo;
+                ImageView category_icon;
+                TextView author;
+
+                public PollViewHolder(View itemView) {
+                    super(itemView);
+                    cv = (CardView) itemView.findViewById(R.id.cv);
+                    question = (TextView) itemView.findViewById(R.id.question);
+                    date = (TextView) itemView.findViewById(R.id.date);
+                    total_votes = (TextView) itemView.findViewById(R.id.category);
+                    cv.setOnClickListener(onclick);
+                    iv = (ImageView) itemView.findViewById(R.id.done);
+                    category_icon = (ImageView) itemView.findViewById(R.id.category_icon);
+                    has_photo = (ImageView) itemView.findViewById(R.id.have_photo);
+                    author = (TextView) itemView.findViewById(R.id.author);
+                    pb = (ProgressBar) itemView.findViewById(R.id.pb);
+                }
+            }
+
+            public RVadapter(List<Poll> pollList) {
+                polls = pollList;
+            }
+
+            @Override
+            public RVadapter.PollViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+                View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.items, viewGroup, false);
+                v.setTag(i);
+                PollViewHolder pvh = new PollViewHolder(v);
+                return pvh;
+            }
+
+            @Override
+            public void onBindViewHolder(RVadapter.PollViewHolder pollViewHolder, int i) {
+                if (polls.get(i).getId().compareTo("-1") == 0) {
+                    pollViewHolder.pb.setVisibility(View.VISIBLE);
+                    pollViewHolder.cv.setVisibility(View.GONE);
+                    return;
+                } else {
+                    pollViewHolder.pb.setVisibility(View.GONE);
+                    pollViewHolder.cv.setVisibility(View.VISIBLE);
+                }
+                Poll p = polls.get(i);
+                pollViewHolder.total_votes.setText(Integer.toString(p.totalVotes()));
+                pollViewHolder.category_icon.setImageResource(Util.drawables[p.getCategory()]);
+                pollViewHolder.date.setText(p.getDate_added());
+                pollViewHolder.question.setText(p.getQuestion());
+                pollViewHolder.cv.setTag(i);
+                if (map.get(p.getId()) != null) {
+                    pollViewHolder.iv.setImageResource(R.drawable.ic_done_green_18dp);
+                } else {
+                    pollViewHolder.iv.setImageResource(R.drawable.ic_done_grey_18dp);
+                }
+                if (p.hasImage() == 1) {
+                    pollViewHolder.has_photo.setVisibility(View.VISIBLE);
+                } else {
+                    pollViewHolder.has_photo.setVisibility(View.GONE);
+                }
+                pollViewHolder.author.setText(p.getAnon() == 1 ? "Anonymous" : p.getAuthor());
+            }
+
+            @Override
+            public void onAttachedToRecyclerView(RecyclerView rv) {
+                super.onAttachedToRecyclerView(rv);
+            }
+
+            @Override
+            public int getItemCount() {
+                return polls.size();
+            }
         }
 
         public SecondFragment() {
 
         }
+
 
     }
 
