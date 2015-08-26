b8f0bc76d3b3dd75cb41d6edf4fb39cc27b7a4ca
tan126 <tan126@purdue.edu>
2015-08-26 12:01:55 -0400

    load more than 10k record data in splash screen

--- app/src/main/java/com/zilche/zilche/SplashScreenActivity.java
+++ app/src/main/java/com/zilche/zilche/SplashScreenActivity.java
@@ -1,14 +1,11 @@
 package com.zilche.zilche;
 
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.parse.FindCallback;
 import com.parse.LogInCallback;
@@ -19,6 +16,7 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -28,6 +26,8 @@ public class SplashScreenActivity extends ActionBarActivity {
     HashMap<String, Integer> map;
     HashMap<String, Integer> fav;
     Zilche app;
+    private Date lastCreated;
+    private Date lastCreated_fav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,14 +85,23 @@ public class SplashScreenActivity extends ActionBarActivity {
         final int s = skip;
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Records");
         query.setLimit(1000);
-        query.setSkip(1000 * s);
+        if (skip % 10 == 0 && map.size() != 0) {
+            query.whereLessThan("createdAt", lastCreated);
+            query.setSkip(skip % 10 * 1000);
+        } else {
+            query.setSkip(1000 * s);
+        }
         query.whereEqualTo("user", ParseUser.getCurrentUser().getObjectId());
+        query.orderByDescending("createdAt");
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> list, ParseException e) {
                 if (e == null) {
                     for (ParseObject o : list) {
                         map.put(o.getString("Key"), o.getInt("choice"));
+                    }
+                    if (list.size() != 0) {
+                        lastCreated = list.get(list.size() - 1).getCreatedAt();
                     }
                     if (list.size() == 1000) {
                         updateMap(s + 1);
@@ -126,14 +135,23 @@ public class SplashScreenActivity extends ActionBarActivity {
     public void updateFav(final int skip) {
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Favourite");
         query.setLimit(1000);
-        query.setSkip(1000 * skip);
+        if (skip % 10 == 0 && fav.size() != 0) {
+            query.whereLessThan("createdAt", lastCreated_fav);
+            query.setSkip(skip % 10 * 1000);
+        } else {
+            query.setSkip(1000 * skip);
+        }
         query.whereEqualTo("user", ParseUser.getCurrentUser().getObjectId());
+        query.orderByDescending("createdAt");
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> list, ParseException e) {
                 if (e == null) {
                     for (ParseObject o : list) {
                         fav.put(o.getString("Key"), o.getInt("Fav"));
+                    }
+                    if (list.size() != 0) {
+                        lastCreated_fav = list.get(list.size() - 1).getCreatedAt();
                     }
                     if (list.size() == 1000) {
                         updateFav(skip + 1);
